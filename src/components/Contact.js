f4d317d8d6915993e2d96597fd4e754578391c04
yungpanko <jrjohns@mit.edu>
2018-07-27 23:05:46 -0400

    cleanup

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ const Contact = (props) => (
     <section id="contact">
         <div className="inner">
             <section>
-                <form name="contact" method="POST" action="/thanks" data-netlify="true">
+                <form name="contact" method="POST" data-netlify="true">
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
                         <input type="text" name="name" id="name" />
